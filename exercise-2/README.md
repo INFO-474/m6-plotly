532ec5b98cb613c81cc528901267a50c97680ddd
Jordan Hand <jordanhand@comcast.net>
2016-04-07 21:14:32 -0700

    Added readme

---
+++ exercise-2/README.md
@@ -0,0 +1,16 @@
+Exercise 2
+----------
+
+##Part 0
+
+The first part of this exercise is just a quick introduction to making
+a chart on plotly. This exercise can also be found on plotly's website
+but I put it here for convenience.
+
+##Part 2
+
+The second part covers a few topics in web development and looks
+more in depth at plotly. There is some starter code that handles
+authentication and page styling but you will be writing most of the
+javascript. Topic covered include REST APIs, AJAX, jQuery, caching
+in JavaScript, and grouped bar charts in plotly.
--- exercise-2/pt1/js/main.js
+++ exercise-2/pt1/js/main.js
@@ -26,12 +26,6 @@ $(document).ready(function() {
 function getUser(username) {
     // Make a reqest to the Github REST api for data about the user
     // with a username stored in the parameter 'username'
-    var userURL = "https://api.github.com/users/" + username;
-    return $.ajax({
-        url: userURL,
-        method: "GET",
-        headers: {Authorization: "Basic " + githubAuth()}
-    });
 }
 
 /*
@@ -41,11 +35,6 @@ function getUser(username) {
 function getRepoCall(user) {
     // After getting the user info, use that data to request from the API
     // the user's github repositories (use a promise to do this) return $.ajax({
-    return $.ajax({
-        url: user.repos_url,
-        method: "GET",
-        headers: {Authorization: "Basic " + githubAuth()}
-    });
 }
 
 /*
@@ -62,83 +51,44 @@ function getRepos(nameA, nameB, callback) {
     //  name: "user",
     //  repos: repoData
     // } 
-    $.when(getUser(nameA), getUser(nameB))
-        .done(function (user1, user2) {
-            $.when(getRepoCall(user1[0]), getRepoCall(user2[0]))
-            .done(function (repos1, repos2) {
-                var data1 = {
-                    name: nameA,
-                    repos: repos1[0]
-                };
-
-                var data2 = {
-                    name: nameB,
-                    repos: repos2[0]
-                };
-                callback(data1, data2);
-            });
-        });
 }
 
 function formatRepoData(repoData) {
     // Make a dictionary mapping languages to total number of forks
     // for that language
-    var boxData = {};
-    repoData.repos.forEach(function(d) {
-        if (d.language != null) {
-            if (boxData[d.language] != undefined) {
-                boxData[d.language] += d.forks;
-            } else {
-                boxData[d.language] = d.forks;
-            }
-        }
-    });
+
+
+
 
     // Format the data so there are two arrays: a list of primary languages
     // and a list of the total forks for each language. This is necessary for
     // making plotly graphs.
-    var keys = Object.keys(boxData);
-    keys.sort();
-    var values = keys.map(function(l) {
-        return boxData[l];
-    });
+    
+
 
     // Using plotly make a bar graph trace with languages on the x axis and
     // The number of forks for repositories with that language as the
     // primary language on the Y-axis. Your function should return this trace.
-    bars = {
-        x: keys,
-        y: values,
-        type: 'bar',
-        name: repoData.name
-    };
-    return bars;
+
+
+
 }
 
 function buildBarGraph(data1, data2) {
 
     // call your formatRepoData function twice to get a trace for
     // each data object and put them in an array.
-    var traces = [formatRepoData(data1), formatRepoData(data2)];
+
 
     // Using a layout object to set the title of the graph
     // and set the mode to be a grouped bar chart. You should
     // also use this to label the x- and y- axis
-    var layout = {
-        title: 'Forks vs Github Repository Primary Language',
-        barmode: 'group',
-        xaxis: {
-            title: "Primary Programming Languages"
-        },
-        yaxis: {
-            title: "# of Forks"
-        },
-        hovermode: 'closest'
-    }
+
+
 
     // call Plotly.newPlot to draw a plotly graph in the dive with an
     // id myDiv with the traces array and layout object you made.
-    Plotly.newPlot('myDiv', traces, layout);
+
 }
 
 function githubAuth() {
